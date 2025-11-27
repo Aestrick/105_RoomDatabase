9a847a0ee081ea014f95854c25a662809ce33c99
Nashrul Fikri <nashrul.fikri19@gmail.com>
2025-11-27 15:56:16 +0700

    Menambahkan data di Siswa.kt

--- app/src/main/java/com/example/pertemuan9/room/Siswa.kt
+++ app/src/main/java/com/example/pertemuan9/room/Siswa.kt
@@ -1,4 +1,13 @@
-package com.example.pertemuan9.room
+package com.example.myroomsatu.room // Sesuaikan dengan nama package project kamu jika berbeda
 
-class Siswa {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tblSiswa")
+data class Siswa(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val nama: String,
+    val alamat: String,
+    val telpon: String
+)
